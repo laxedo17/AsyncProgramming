f8608dfc13bd97effa7a493c7812dc3de87bfc19
laxedo17 <saracyan78@hotmail.com>
2021-06-12 00:46:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Async1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Demonstracion-1.A Programa ASyncrono.***");
+            Metodo1();
+            Metodo2();
+            Console.WriteLine("Finalizando Main()");
+            Console.ReadKey();
+        }
+
+        private static void Metodo1()
+        {
+            Console.WriteLine("Metodo1() comenzou.");
+            // Unha tarefa grande
+            Thread.Sleep(1000);
+            Console.WriteLine("Metodo1() finalizou");
+        }
+
+        private static void Metodo2()
+        {
+            Console.WriteLine("Metodo2() comenzou.");
+            // Unha tarefa pequena
+            Thread.Sleep(100);
+            Console.WriteLine("Metodo2() finalizou");
+        }
+    }
+}
